8fb3a22f47fd7e604156c68cb0ff9eff93f7eb5b
JHS1790 <1790@holbertonschool.com>
2020-09-01 19:54:32 +0000

    trying again

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 def remove_char_at(str, n):
-    if n > len(str) or n < 0:
-        return(str)
-#    cpstr = ''
-#    for x in range(len(str)):
-#        if not x == n:
-#            cpstr += str[x]
-#        else:
-#            pass
-#    return cpstr
-    return str.replace(str[n], '')
+#    if n > len(str) or n < 0:
+#        return(str)
+#    return str.replace(str[n], '')
+    cpstr = ''
+    for x in range(len(str)):
+        if not x == n:
+            cpstr += str[x]
+        else:
+            pass
+    return cpstr
