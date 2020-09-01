5e07cc0948c350d4154310364b5007454ca288ff
JHS1790 <1790@holbertonschool.com>
2020-09-01 19:14:52 +0000

    trying completely different logic

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+#def uppercase(str):
+#    cpstr = ""
+#    for x in range(0, len(str)):
+#        oldascval = ord(str[x])
+#        if oldascval >= 97 and oldascval <= 122:
+#            newascval = oldascval - 32
+#            cpstr = cpstr + chr(newascval)
+#        else:
+#            cpstr = cpstr + chr(oldascval)
+#    print(cpstr)
 def uppercase(str):
-    cpstr = ""
-    for x in range(0, len(str)):
-        oldascval = ord(str[x])
-        if oldascval >= 97 and oldascval <= 122:
-            newascval = oldascval - 32
-            cpstr = cpstr + chr(newascval)
-        else:
-            cpstr = cpstr + chr(oldascval)
-    print(cpstr)
+    for letter in str:
+        if ord(letter) >= ord('a') and ord(letter) <= ord('z'):
+            letter = ord(letter) - 32
+            letter = chr(letter)
+        print("{}".format(letter), end='')
+    print('')
