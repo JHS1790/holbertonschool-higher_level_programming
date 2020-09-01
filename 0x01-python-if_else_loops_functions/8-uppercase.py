cf0ce8f35f011fdc60a5a6b77bb28f26addc9ca5
JHS1790 <1790@holbertonschool.com>
2020-09-01 19:18:08 +0000

    back to square one

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-#def uppercase(str):
-#    cpstr = ""
-#    for x in range(0, len(str)):
-#        oldascval = ord(str[x])
-#        if oldascval >= 97 and oldascval <= 122:
-#            newascval = oldascval - 32
-#            cpstr = cpstr + chr(newascval)
-#        else:
-#            cpstr = cpstr + chr(oldascval)
-#    print(cpstr)
 def uppercase(str):
-    for letter in str:
-        if ord(letter) >= ord('a') and ord(letter) <= ord('z'):
-            letter = ord(letter) - 32
-            letter = chr(letter)
-        print("{}".format(letter), end='')
-    print('')
+    cpstr = ""
+    for x in range(0, len(str)):
+        oldascval = ord(str[x])
+        if oldascval >= 97 and oldascval <= 122:
+            newascval = oldascval - 32
+            cpstr = cpstr + chr(newascval)
+        else:
+            cpstr = cpstr + chr(oldascval)
+    print(cpstr)
