84c69e8b55e5845631c33dd813ee313ba30b27f3
JHS1790 <1790@holbertonschool.com>
2020-09-03 16:25:16 +0000

    First draft on task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sys import argv
+argc = len(argv) - 1
+i = 1
+if argc == 0:
+    print("{} arguments.".format(argc))
+elif argc == 1:
+    print("{} argument:".format(argc))
+    while i <= argc:
+        print("{}: {}".format(i, argv[i]))
+        i += 1
+else:
+    print("{} arguments:".format(argc))
+    while i <= argc:
+        print("{}: {}".format(i, argv[i]))
+        i += 1
