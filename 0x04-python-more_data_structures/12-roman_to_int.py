18339a84557df9257c92a4ee3384604422c1af59
JHS1790 <1790@holbertonschool.com>
2020-09-10 19:08:53 +0000

    pep8 review

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string == None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     roman_list = list(roman_string)
     arabic_number = 0
     roman_dict = {
-        'I':1, 'IV':4, 'V':5, 'IX':9,
-        'X':10, 'XL':40, 'L':50, 'XC':90,
-        'C':100, 'CD':400, 'D':500, 'CM':900,
-        'M':1000
+        'I': 1, 'IV': 4, 'V': 5, 'IX': 9,
+        'X': 10, 'XL': 40, 'L': 50, 'XC': 90,
+        'C': 100, 'CD': 400, 'D': 500, 'CM': 900,
+        'M': 1000
     }
     x = 0
     while x < len(roman_list):
