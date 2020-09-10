dac60d044720144debb4945f53b6306efbe47c77
JHS1790 <1790@holbertonschool.com>
2020-09-10 19:04:25 +0000

    improved internal logic

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,19 +6,20 @@ def roman_to_int(roman_string):
     arabic_number = 0
     roman_dict = {
         'I':1, 'IV':4, 'V':5, 'IX':9,
-        'X':10, 'L':50, 'C':100, 'D':500,
+        'X':10, 'XL':40, 'L':50, 'XC':90,
+        'C':100, 'CD':400, 'D':500, 'CM':900,
         'M':1000
     }
     x = 0
     while x < len(roman_list):
-        if roman_list[x] is not 'I':
-            arabic_number += roman_dict[roman_list[x]]
-        else:
-            if x == len(roman_list) - 1 or roman_list[x + 1] == 'I':
-                arabic_number += 1
-            else:
+        if x < len(roman_list) - 1:
+            if roman_dict[roman_list[x + 1]] > roman_dict[roman_list[x]]:
                 cato = str(roman_list[x] + roman_list[x + 1])
                 arabic_number += roman_dict[cato]
                 x += 1
+            else:
+                arabic_number += roman_dict[roman_list[x]]
+        else:
+            arabic_number += roman_dict[roman_list[x]]
         x += 1
     return arabic_number
