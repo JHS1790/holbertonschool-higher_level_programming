a00b21c6c7f5d50d1594d1b416590d9df61318d8
JHS1790 <1790@holbertonschool.com>
2020-09-10 17:52:24 +0000

    first draft on task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    best_student = ''
+    best_score = 0
+    for key in a_dictionary:
+        if a_dictionary[key] >= best_score:
+            best_student = key
+            best_score = a_dictionary[key]
+    return best_student
