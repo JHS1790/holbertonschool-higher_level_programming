ee2735b38f5ef82af89e6f6d99f3a0fceb314cce
JHS1790 <1790@holbertonschool.com>
2020-09-22 20:44:12 +0000

    pep8

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """file for class square"""
+
+
 class Square:
         """class to build a square
 
-    Attributes:
+        Attributes:
         size (int): size of the square
         position (tuple): position of the square down and right
-    """
-    def __init__(self, size=0):
-        """__init__ function for the Square class
-
-        Args:
-            size (int): size of the square, attribute assignment
-            position (tuple): position of the square, attribute assignment
         """
-        self.__size = size
+        def __init__(self, size=0):
+                """__init__ function for the Square class
+
+                Args:
+                size (int): size of the square, attribute assignment
+                position (tuple): position of the square, attribute assignment
+                """
+                self.__size = size
