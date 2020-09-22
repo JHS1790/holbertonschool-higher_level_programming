0c6b1c6be231a3d12516fef0ae0bf665b28900d1
JHS1790 <1790@holbertonschool.com>
2020-09-22 17:06:58 +0000

    first draft on task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.ssize = size
+
+    @property
+    def ssize(self):
+        return self.__size
+
+    @ssize.setter
+    def ssize(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.ssize * self.ssize
