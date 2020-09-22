d41928fb3cd7478fbd59f954e07333f1e3831871
JHS1790 <1790@holbertonschool.com>
2020-09-22 20:39:02 +0000

    documentation for task 1 updated

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""file for class square"""
 class Square:
+        """class to build a square
+
+    Attributes:
+        size (int): size of the square
+        position (tuple): position of the square down and right
+    """
     def __init__(self, size=0):
+        """__init__ function for the Square class
+
+        Args:
+            size (int): size of the square, attribute assignment
+            position (tuple): position of the square, attribute assignment
+        """
         self.__size = size
