ebf8bfef358060fdf70ad1503ca108a2d6930e36
JHS1790 <1790@holbertonschool.com>
2020-09-22 17:48:38 +0000

    first attempt at documentation

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python3
 class Square:
+    """class to build a square
+
+    Attributes:
+        size (int): size of the square
+        position (tuple): position of the square down and right
+    """
     def __init__(self, size=0, position=(0,0)):
+    """__init__ function for the Square class
+
+    Args:
+        size (int): size of the square, attribute assignment
+        position (tuple): position of the square, attribute assignment
+    """
         self.size = size
         self.position = position
 
     @property
+    """int: size of the square"""
     def size(self):
         return self.__size
 
     @size.setter
+    """
+    Raises:
+        TypeError: if value is not an int
+        ValueError: if value is less than zero
+    """
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -18,15 +36,20 @@ class Square:
             self.__size = value
 
     @property
+    """tuple: position of square right and down"""
     def position(self):
         return self.__position
 
     @position.setter
+    """
+    Raises:
+        TypeError: if value is not a tuple of two positive integers
+    """
     def position(self, value):
         isit = True
         if type(value) is tuple:
-            if isinstance(value[0], int):
-                if isinstance(value[1], int):
+            if isinstance(value[0], int) and value[0] > 0:
+                if isinstance(value[1], int) and value[1] > 0:
                     self.__position = value
                 else:
                     isit = False
@@ -38,9 +61,11 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
+        """method to find area of square"""
         return self.size * self.size
 
     def my_print(self):
+        """method to print square"""
         for a in range(self.position[1]):
             print()
         for x in range(self.size):
