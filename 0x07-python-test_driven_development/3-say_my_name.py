f30bb6e121ae82a4e51a499a1ed71bc79b147487
JHS1790 <1790@holbertonschool.com>
2020-09-24 19:26:44 +0000

    documentation update on task 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""2. Say my name"""
 def say_my_name(first_name, last_name=""):
+    """says the name
+
+    Args:
+    first_name: string
+    last_name: string, defaults to blank
+
+    Exceptions:
+    TypeError: if first_name or last_name is not a string
+
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
