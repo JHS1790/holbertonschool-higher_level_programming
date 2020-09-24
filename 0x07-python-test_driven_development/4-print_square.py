2d1d26390a144e680b1ab6af224c1472c5b03e19
JHS1790 <1790@holbertonschool.com>
2020-09-24 19:25:09 +0000

    documentation update on task 3

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""3. Print square"""
 def print_square(size):
+    """prints a square
+
+    Args:
+    size: integer or float indicating final size of square
+
+    Exceptions:
+    TypeError: if size is not an integer or float, or if a negative float
+    ValueError: if size is negative
+
+    """
     if not isinstance(size, int) and not isinstance(size, float):
         raise TypeError("size must be an integer")
     if isinstance(size, float) and size < 0:
