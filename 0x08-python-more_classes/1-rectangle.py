ccf7c36dc05fd0ba9afe4f25c4a05218e9381fe0
JHS1790 <1790@holbertonschool.com>
2020-09-28 16:11:23 +0000

    first draft on task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""This file is NEVER to be placed in a manila folder, on pain of termination"""
+
+
+class Rectangle:
+    """A class for the exposition and evangalization of the TRIANGLe, wait no,
+    its a rectangle now, damn
+
+    Attributes:
+    width (int): the width of the rectangle
+    height (int): the height of the rectangle
+    """
+
+    def __init__(self, width = 0, height = 0):
+        """Initiate rectangular launch sequence, Vassily
+
+        Args:
+        width (int): expected width of square
+        height (int): expected height of square
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for width
+
+        Args:
+        value: int to make the width
+
+        Raises:
+        TypeError: if value is not an int
+        ValueError: if value is negative
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for height
+
+        Args:
+        value: int to make the height
+
+        Raises:
+        TypeError: if value is not an int
+        ValueError: if value is negative
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
