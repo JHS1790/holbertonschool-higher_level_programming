6c9b32b5c658e299b3778020bca82ea948b23426
JHS1790 <1790@holbertonschool.com>
2020-09-28 15:49:18 +0000

    first draft on task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This file is NEVER to be placed in a manila folder, on pain of termination"""
+class Rectangle:
+    """A class for the exposition and evangalization of the TRIANGLe, wait no,
+    its a rectangle now, damn"""
+
+    pass
