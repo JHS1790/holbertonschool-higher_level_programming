38d167c2d7a6b36903261abdd85f91198bd70c83
JHS1790 <1790@holbertonschool.com>
2020-09-30 14:27:31 +0000

    second draft on task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """0. Integers addition"""
+
+
 def add_integer(a, b=98):
     """Adds two integers
     Args:
@@ -12,18 +14,6 @@ def add_integer(a, b=98):
     Returns:
     addition of a and b
 
-    Examples:
-
-    >>> add_integer(10, 20)
-    30
-
-    >>> add_integer(9.5, 30)
-    39
-
-    >>> add_integer("Johnny", "Jane")
-    Traceback (most recent call last):
-    ...
-    TypeError: a must be an integer
     """
     if isinstance(a, float):
         a = int(a)
