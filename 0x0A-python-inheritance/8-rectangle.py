32c504f7ef545153c07cbfd01fc9bf4e1059be66
JHS1790 <1790@holbertonschool.com>
2020-10-06 17:10:22 +0000

    first draft on task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""file"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+bg = BaseGeometry()
+class Rectangle(BaseGeometry):
+    """class"""
+
+    def __init__(self, width, height):
+        """method"""
+        bg.integer_validator("width", width)
+        bg.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
