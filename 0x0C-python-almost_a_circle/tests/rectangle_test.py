8397364cbd21fe8673b3c4c9e63502b471f10783
JHS1790 <1790@holbertonschool.com>
2020-10-12 16:35:06 +0000

    first draft on task 3

--- 0x0C-python-almost_a_circle/tests/rectangle_test.py
+++ 0x0C-python-almost_a_circle/tests/rectangle_test.py
@@ -6,7 +6,7 @@ from models.rectangle import Rectangle
 
 
 class TestRectangleCreation(unittest.TestCase):
-    """TestRectangle: Test the creation of the Rectangle class"""
+    """TestRectangleCreation: Test the creation of the Rectangle class"""
 
     def test_id(self):
         """test_id: method for testing Rectangle class id assignment"""
@@ -36,3 +36,29 @@ class TestRectangleCreation(unittest.TestCase):
         """test_y: method for testing Rectangle class y assignment"""
         recy = Rectangle(3, 4, 1, 2)
         self.assertAlmostEqual(recy.y, 2)
+
+
+class TestRectangleFailure(unittest.TestCase):
+    """TestRectangleFailure: Test raised exceptions from Rectangle class
+    creation
+    """
+
+    def test_width_failure(self):
+        """test_width_failure: test width exceptions"""
+        with self.assertRaises(TypeError): Rectangle("boi", 3)
+        with self.assertRaises(ValueError): Rectangle(-5, 2)
+
+    def test_height_failure(self):
+        """test_height_failure: test height exceptions"""
+        with self.assertRaises(TypeError): Rectangle(2, [])
+        with self.assertRaises(ValueError): Rectangle(4, -5)
+
+    def test_x_failure(self):
+        """test_x_failure: test x exceptions"""
+        with self.assertRaises(TypeError): Rectangle(1, 2, 'blue', 4)
+        with self.assertRaises(ValueError): Rectangle(1, 2, -3, 4)
+
+    def test_y_failure(self):
+        """test_y_failure: test y exceptions"""
+        with self.assertRaises(TypeError): Rectangle(9, 8, 7, {})
+        with self.assertRaises(ValueError): Rectangle(4, 5, 6, -7)
