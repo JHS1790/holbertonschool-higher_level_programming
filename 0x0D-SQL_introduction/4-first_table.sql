ff66e3a450484dc883d6270d1ca00efbf2ecd062
JHS1790 <1790@holbertonschool.com>
2020-11-16 18:03:20 +0000

    made int straight instead of dynamic

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,3 @@
 -- a script that creates a table called first_table in the current database in MySQL server
 -- creates the table
-CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT(11), name VARCHAR(256));
