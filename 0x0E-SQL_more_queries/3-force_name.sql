aefdf234930af44b2e23a333d1d256c14d62cbf2
JHS1790 <1790@holbertonschool.com>
2020-11-17 16:23:10 +0000

    first go at 3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- a script that creates the table force_name on your MySQL server
+-- creates table
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+    );
+-- ); is crying because SQL sucks
