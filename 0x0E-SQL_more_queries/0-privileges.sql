a82c02f56a3f8e0b79e89214d367e9e3dc129fcd
JHS1790 <1790@holbertonschool.com>
2020-11-17 15:53:37 +0000

    added comment

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
 -- lets see now
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- ok I think that did it
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
