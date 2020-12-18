86cbe1725ed63140ffda6523ed3834187d4aeb34
JHS1790 <1790@holbertonschool.com>
2020-12-18 20:24:04 +0000

    go at 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""6. Find a peak"""
+
+def find_peak(list_of_integers):
+	"""function that finds a peak in a list of unsorted integers."""
+	if list_of_integers:
+		n = list_of_integers[0]
+		for x in range(len(list_of_integers)):
+			if list_of_integers[x] > n:
+				n = list_of_integers[x]
+		return n
+	return None
