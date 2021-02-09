c6cd299bedc3acc0258dc326a9140bc989b8d8bb
JHS1790 <1790@holbertonschool.com>
2021-02-09 18:32:10 +0000

    switched around err and data fo 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (data) {
-    console.log(data);
-  } else {
+  if (err) {
     return console.log(err);
+  } else {
+    console.log(data);
   }
 });
