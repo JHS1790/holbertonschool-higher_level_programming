71b2f582c2418149b446b7b304cf079d34d1c93c
JHS1790 <1790@holbertonschool.com>
2021-02-11 17:02:07 +0000

    first go at 1

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,7 @@
+/* script that updates the text color of the HTML tag HEADER to red (#FF0000): */
+
+const $ = window.$;
+
+$(document).ready(function () {
+  $('header').css({ color: '#FF0000' });
+});
