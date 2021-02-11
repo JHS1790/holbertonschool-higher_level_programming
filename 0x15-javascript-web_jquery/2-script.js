5fa51e80423bf304c296cff3e074e0e4f0c90b30
JHS1790 <1790@holbertonschool.com>
2021-02-11 17:06:27 +0000

    first go at 2

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,12 @@
+/*
+script that updates the text color of the HTML tag HEADER
+to red (#FF0000) when user click on DIV#red_header
+*/
+
+const $ = window.$;
+
+$(document).ready(function () {
+  $('DIV#red_header').click(function () {
+    $('HEADER').css({ color: '#FF0000' });
+  });
+});
