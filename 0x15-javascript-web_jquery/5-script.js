9400d21edb4ae449c21de88ab30d5fcb93c7a64d
JHS1790 <1790@holbertonschool.com>
2021-02-11 18:01:27 +0000

    first go at 5

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,9 @@
+/* script that adds a LI element to a list when the user clicks on the tag DIV#add_item */
+
+const $ = window.$;
+
+$(document).ready(function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+});
