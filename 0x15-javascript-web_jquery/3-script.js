e7790f0c0677948b8718840668152ac3b6ed7f99
JHS1790 <1790@holbertonschool.com>
2021-02-11 17:13:35 +0000

    first go at 3

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,9 @@
+/* script that adds the class red to the HTML tag HEADER when the user clicks on the tag DIV#red_header */
+
+const $ = window.$;
+
+$(document).ready(function () {
+  $('DIV#red_header').click(function () {
+    $('HEADER').addClass('red');
+  });
+});
